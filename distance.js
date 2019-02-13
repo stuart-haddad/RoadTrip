dedbfdfefa1ac55ef858856922b0281681d20f06
Stu <stuart.haddad@gmail.com>
2019-02-13 12:51:32 -0600

    Add callback to distance.js

--- distance.js
+++ distance.js
@@ -17,6 +17,19 @@ service.getDistanceMatrix(
   }, callback);
 
 function callback(response, status) {
-  // See Parsing the Results for
-  // the basics of a callback function.
+  if (status == 'OK') {
+     var origins = response.originAddresses;
+     var destinations = response.destinationAddresses;
+
+     for (var i = 0; i < origins.length; i++) {
+       var results = response.rows[i].elements;
+       for (var j = 0; j < results.length; j++) {
+         var element = results[j];
+         var distance = element.distance.text;
+         var duration = element.duration.text;
+         var from = origins[i];
+         var to = destinations[j];
+       }
+     }
+   }
 }
