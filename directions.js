65905138b20a01971b61d8adae3acb84973b25f7
Stu <stuart.haddad@gmail.com>
2019-02-14 12:19:47 -0600

    Add Directions API Map, Consolidate Styles

---
+++ directions.js
@@ -0,0 +1,107 @@
+function initMap() {
+  var directionsService = new google.maps.DirectionsService();
+  var directionsDisplay = new google.maps.DirectionsRenderer();
+  // The location of Baton Rouge
+  var batonRouge = new google.maps.LatLng(30.4515, -91.1871);
+  var mapOptions = {
+    zoom: 10,
+    center: batonRouge,
+    styles: [
+       {elementType: 'geometry', stylers: [{color: '#A5E39E'}]},
+       {elementType: 'labels.text.stroke', stylers: [{color: '#123C5C'}]},
+       {elementType: 'labels.text.fill', stylers: [{color: '#9ca5b3'}]},
+       {
+         featureType: 'administrative.locality',
+         elementType: 'labels.text.fill',
+         stylers: [{color: '#9ca5b3'}]
+       },
+       {
+         featureType: 'poi',
+         elementType: 'labels.text.fill',
+         stylers: [{color: '#9ca5b3'}]
+       },
+       {
+         featureType: 'poi.park',
+         elementType: 'geometry',
+         stylers: [{color: '#72CD68'}]
+       },
+       {
+         featureType: 'poi.park',
+         elementType: 'labels.text.fill',
+         stylers: [{color: '#9ca5b3'}]
+       },
+       {
+         featureType: 'road',
+         elementType: 'geometry',
+         stylers: [{color: '#D6F3D3'}]
+       },
+       {
+         featureType: 'road',
+         elementType: 'geometry.stroke',
+         stylers: [{color: '#D6F3D3'}]
+       },
+       {
+         featureType: 'road',
+         elementType: 'labels.text.fill',
+         stylers: [{color: '#9ca5b3'}]
+       },
+       {
+         featureType: 'road.highway',
+         elementType: 'geometry',
+         stylers: [{color: '#D6F3D3'}]
+       },
+       {
+         featureType: 'road.highway',
+         elementType: 'geometry.stroke',
+         stylers: [{color: '#D6F3D3'}]
+       },
+       {
+         featureType: 'road.highway',
+         elementType: 'labels.text.fill',
+         stylers: [{color: '#9ca5b3'}]
+       },
+       {
+         featureType: 'transit',
+         elementType: 'geometry',
+         stylers: [{color: '4E738F'}]
+       },
+       {
+         featureType: 'transit.station',
+         elementType: 'labels.text.fill',
+         stylers: [{color: '#9ca5b3'}]
+       },
+       {
+         featureType: 'water',
+         elementType: 'geometry',
+         stylers: [{color: '#57B8CF'}]
+       },
+       {
+         featureType: 'water',
+         elementType: 'labels.text.fill',
+         stylers: [{color: '#515c6d'}]
+       },
+       {
+         featureType: 'water',
+         elementType: 'labels.text.stroke',
+         stylers: [{color: '#17263c'}]
+       }
+     ]};
+  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  var marker = new google.maps.Marker({position: batonRouge, map: map});
+  directionsDisplay.setMap(map);
+}
+
+function calcRoute() {
+  var start = document.getElementById('start').value;
+  var end = document.getElementById('end').value;
+  var request = {
+    origin: start,
+    destination: end,
+    travelMode: 'DRIVING'
+  };
+  directionsService.route(request, function(result, status) {
+    if (status == 'OK') {
+      directionsDisplay.setDirections(result);
+    }
+  });
+}
