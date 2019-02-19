3c43e5fb09895053fabf987c8e6775e3c7f9946c
cmosol <46791335+cmosol@users.noreply.github.com>
2019-02-18 21:24:27 -0600

    Create InterestQuery.py

---
+++ InterestQuery.py
@@ -0,0 +1,22 @@
+class InterestQuery:
+
+    interests = [];
+
+        def __init__(self):
+            pass;
+
+        def log(self):
+            pass;
+
+        def related_interests(self):
+            pass;
+
+        def display(self, interests):
+            for i in interests-1:
+                print("your interests are: "interests[i] + ", ");
+            print (interests[i]);
+
+        def add_choice(self, choice):
+            self.interests.__add__(choice);
+
+
