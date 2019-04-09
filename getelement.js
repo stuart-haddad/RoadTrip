9e39cecf7f3d15b3a008d2917f73881ae60f75a0
jsvec1 <37349200+jsvec1@users.noreply.github.com>
2019-04-09 13:14:52 -0500

    make function for getting elements

---
+++ getelement.js
@@ -0,0 +1,8 @@
+function myFunction()
+{
+  var x = document.getElementById("end");
+  var text = "";
+  // for (i = 0; i < x.length ;i++) {
+  text += x.value + "<br>";
+  document.getElementById("demo").innerHTML = text;
+ }
